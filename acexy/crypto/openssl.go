ef6fd2680e224ce3ec5d61ab81c768daac804e4e
Acexy <thankjava@gmail.com>
2025-05-29 18:38:57 +0800

    源码配置加密

--- acexy/crypto/openssl.go
+++ acexy/crypto/openssl.go
@@ -18,27 +18,20 @@ func DecryptOpenSSL(cipherData []byte, password string) ([]byte, error) {
 		return nil, errors.New("invalid openssl salted format")
 	}
 	salt := header[8:]
-
-	// 派生 Key 和 IV
 	key := pbkdf2.Key([]byte(password), salt, 10000, 32+16, sha256.New)
 	aesKey := key[:32]
 	iv := key[32:]
-
 	block, err := aes.NewCipher(aesKey)
 	if err != nil {
 		return nil, err
 	}
-
 	ciphertext := cipherData[16:]
 	if len(ciphertext)%aes.BlockSize != 0 {
 		return nil, errors.New("ciphertext is not a multiple of the block size")
 	}
-
 	mode := cipher.NewCBCDecrypter(block, iv)
 	plaintext := make([]byte, len(ciphertext))
 	mode.CryptBlocks(plaintext, ciphertext)
-
-	// 去除 PKCS#7 padding
 	padding := int(plaintext[len(plaintext)-1])
 	if padding == 0 || padding > aes.BlockSize {
 		return nil, errors.New("invalid padding")
